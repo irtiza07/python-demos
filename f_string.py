ff02d0168cca99ea4f859895625bd148d62ff1a6
Irtiza Hafiz <irtiza@irtiza.local>
2022-01-11 14:08:52 -0800

    formatted strings

--- f_string.py
+++ f_string.py
@@ -1,42 +1,42 @@
-# name = 'Irtiza'
-# age = 25
-# profession = 'software engineer'
-# city = 'San Francisco'
+name = 'Irtiza'
+age = 25
+profession = 'software engineer'
+city = 'San Francisco'
 
-# upper = "SAN FRANCISCO"
+upper = "SAN FRANCISCO"
 
-# Simple Example
-# print(f"I am a {age} years old {profession} living in {city}")
+Simple Example
+print(f"I am a {age} years old {profession} living in {city}")
 
-# Evaluate expressions at runtime
-# print(f"Ten years from now, I will be {age+10} years old")
-# print(f"{upper.lower()}")
+Evaluate expressions at runtime
+print(f"Ten years from now, I will be {age+10} years old")
+print(f"{upper.lower()}")
 
-# # Look at variables
-# print(f"{name=}")
-# print(f"{age=}")
-# print(f"{profession=}")
-# print(f"{city=}")
+# Look at variables
+print(f"{name=}")
+print(f"{age=}")
+print(f"{profession=}")
+print(f"{city=}")
 
-# # Look at Objects
-# class Car:
-#     def __init__(self, make, year):
-#         self.make = make
-#         self.year = year
+# Look at Objects
+class Car:
+    def __init__(self, make, year):
+        self.make = make
+        self.year = year
     
-#     def __repr__(self) -> str:
-#         return f"{self.make}:{self.year}"
+    def __repr__(self) -> str:
+        return f"{self.make}:{self.year}"
 
-# car_1 = Car("Ford", 2020)
-# car_2 = Car("Ferrari", 1991)
-# print(car_1)
-# print(car_2)
+car_1 = Car("Ford", 2020)
+car_2 = Car("Ferrari", 1991)
+print(car_1)
+print(car_2)
 
-# # Work with dictionaries
-# my_data = {
-#     'name': 'Irtiza',
-#     'age': 25,
-#     'profession': 'software engineer',
-#     'city': 'San Francisco'
-# }
-# print(f"I am a {my_data['age']} years old {my_data['profession']} living in {my_data['city']}")
+# Work with dictionaries
+my_data = {
+    'name': 'Irtiza',
+    'age': 25,
+    'profession': 'software engineer',
+    'city': 'San Francisco'
+}
+print(f"I am a {my_data['age']} years old {my_data['profession']} living in {my_data['city']}")
